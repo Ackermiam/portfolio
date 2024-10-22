0a6aa1b926754ee4f6ffab390db08289e7191891
unknown <wullaertbrian@gmail.com>
2024-10-22 22:48:49 +0200

    add background to thirdsection

--- src/components/ThirdSection.vue
+++ src/components/ThirdSection.vue
@@ -6,11 +6,13 @@
 
 <script setup lang="ts">
 
+
 </script>
 
 <style scoped lang="scss">
 .ThirdSection {
   height: 100vh;
+  background: linear-gradient(190deg, rgba(0,0,0,1) 25%, rgb(79, 31, 90) 50%, rgba(0,0,0,1) 75%);
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -18,32 +20,6 @@
   @media (min-width: 900px) {
     flex-direction: row;
     align-items: center;
-  }
-
-  &__Mesh {
-    width: 100%;
-    height: 100%;
-
-    @media (min-width: 900px) {
-      width: 50%;
-    }
-  }
-
-  &__Text {
-    width: 50%;
-  }
-}
-
-p {
-  padding: 0 6vw;
-  color: white;
-  font-size: 2vw;
-  margin: 0;
-  font-family: "Nostal";
-  max-width: 100%;
-
-  @media (min-width: 900px) {
-    font-size: 1.5vw;
   }
 }
 </style>
