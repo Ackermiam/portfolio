aed06c4227d8d203fc6fd69fbb1d478b0096e441
unknown <wullaertbrian@gmail.com>
2024-10-27 22:14:52 +0100

    update intersection component

--- src/three/cubes.ts
+++ src/three/cubes.ts
@@ -10,16 +10,15 @@ import {
   PointLight
 } from "three";
 
-import { type Ref } from "vue";
-
 export class Cubes {
   renderer: WebGLRenderer;
   scene: Scene;
   camera: PerspectiveCamera;
-  mesh: Object3D;
+  mesh: Object3D[];
 
   constructor(ref: HTMLCanvasElement) {
     this.scene = new Scene();
+    this.mesh = [];
 
     const { width, height } = ref.getBoundingClientRect();
     this.camera = new PerspectiveCamera(
@@ -31,7 +30,7 @@ export class Cubes {
     this.renderer = new WebGLRenderer({ antialias: true });
     this.renderer.setSize(width, height);
 
-    const geometry = new BoxGeometry(1, 1, 1);
+    const geometry = new BoxGeometry(.4, .4, .4);
     const material = new MeshPhongMaterial({ color: 0xff8ff4, shininess: 70, specular: 0xffabf7});
 
     const directionalLight = new DirectionalLight(0xffffff, 3);
@@ -39,22 +38,28 @@ export class Cubes {
     this.scene.add(directionalLight);
 
     const pointLight1 = new PointLight(0x96fff8, 10, 100);
-    pointLight1.position.set(0, -5, 5);
+    pointLight1.position.set(0, 0, 5);
     this.scene.add(pointLight1);
 
     const pointLight2 = new PointLight(0xffffff, 10, 100);
-    pointLight2.position.set(5, 5, 5);
+    pointLight2.position.set(5, 0, 5);
     this.scene.add(pointLight2);
 
     const pointLight3 = new PointLight(0xffffff, 10, 100);
-    pointLight3.position.set(-5, 5, 5);
+    pointLight3.position.set(-5, 0, 5);
     this.scene.add(pointLight3);
 
-    const cube = new Mesh(geometry, material);
-    this.mesh = cube;
+    for(let i = -15; i < 15; i += 0.7) {
+      //x = -15 / y = -15
+      for(let j = -15; j < 15; j += 0.7) {
+        const cube = new Mesh(geometry, material);
+        cube.position.set(i, j, 0);
+        this.mesh.push(cube);
+      }
+    }
     console.log(this.mesh);
 
-    this.scene.add(this.mesh);
+    this.scene.add(...this.mesh);
 
     ref.appendChild(this.renderer.domElement);
 
@@ -75,10 +80,10 @@ export class Cubes {
   }
 
   move() {
-    this.mesh.rotation.x += 0.005;
+    this.mesh.forEach(e => e.rotateY(0.005))
   }
 
   moveOnScroll() {
-    this.mesh.rotation.y += 0.02;
+    this.mesh.forEach(e => e.rotateY(0.02))
   }
 }
