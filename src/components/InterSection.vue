e785aeaa8ac458e24851a2ce0653f1f3ae1b382d
unknown <67873431+Ackermiam@users.noreply.github.com>
2024-10-31 18:36:38 +0100

    prettier

--- src/components/InterSection.vue
+++ src/components/InterSection.vue
@@ -1,18 +1,16 @@
 <template>
-  <section class="InterSection" ref="instancedCubes">
-
-  </section>
+  <section class="InterSection" ref="instancedCubes"></section>
 </template>
 
 <script setup lang="ts">
-import {ref, onMounted} from "vue";
-import {Cubes} from "../three/cubes";
+import { ref, onMounted } from "vue";
+import { Cubes } from "../three/cubes";
 
 const instancedCubes = ref();
 
 onMounted(() => {
   const cubes = new Cubes(instancedCubes.value);
-})
+});
 </script>
 
 <style scoped lang="scss">
